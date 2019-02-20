62ad7fd141d7762438c4219b0b813464a2149f00
Simon Mitchell <simon@3sidedcube.com>
2019-02-20 17:15:05 +0000

    Adds remove percent encoding to private key

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,13 +5,13 @@ import FCM
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     
-    let fernoConfig = FernoConfig(basePath: Environment.get("FIREBASE_DATABASE_URL") ?? "", email: Environment.get("FIREBASE_CLIENT_EMAIL") ?? "", privateKey: Environment.get("FIREBASE_PRIVATE_KEY")?.replacingOccurrences(of: "\\n", with: "\n") ?? "")
+    let fernoConfig = FernoConfig(basePath: Environment.get("FIREBASE_DATABASE_URL") ?? "", email: Environment.get("FIREBASE_CLIENT_EMAIL") ?? "", privateKey: Environment.get("FIREBASE_PRIVATE_KEY")?.replacingOccurrences(of: "\\n", with: "\n").removingPercentEncoding ?? "")
     services.register(fernoConfig)
     try services.register(FernoProvider())
     
     let fcm = FCM(email: Environment.get("FIREBASE_CLIENT_EMAIL") ?? "",
                   projectId: Environment.get("FIREBASE_PROJECT_ID") ?? "",
-                  key: Environment.get("FIREBASE_PRIVATE_KEY")?.replacingOccurrences(of: "\\n", with: "\n") ?? "")
+                  key: Environment.get("FIREBASE_PRIVATE_KEY")?.replacingOccurrences(of: "\\n", with: "\n").removingPercentEncoding ?? "")
     services.register(fcm, as: FCM.self)
     
     let slackConfig = SlackConfig(basePath: Environment.get("SLACK_WEBHOOK_URL") ?? "")
