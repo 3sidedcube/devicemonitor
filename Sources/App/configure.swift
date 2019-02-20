57d0feccf3dd0e076bb2044f1e7306d63c52a5b5
Simon Mitchell <simon@3sidedcube.com>
2019-02-20 17:09:34 +0000

    Allows for firebase private key with escaped line-breaks in

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,13 +5,13 @@ import FCM
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     
-    let fernoConfig = FernoConfig(basePath: Environment.get("FIREBASE_DATABASE_URL") ?? "", email: Environment.get("FIREBASE_CLIENT_EMAIL") ?? "", privateKey: Environment.get("FIREBASE_PRIVATE_KEY") ?? "")
+    let fernoConfig = FernoConfig(basePath: Environment.get("FIREBASE_DATABASE_URL") ?? "", email: Environment.get("FIREBASE_CLIENT_EMAIL") ?? "", privateKey: Environment.get("FIREBASE_PRIVATE_KEY")?.replacingOccurrences(of: "\\n", with: "\n") ?? "")
     services.register(fernoConfig)
     try services.register(FernoProvider())
     
     let fcm = FCM(email: Environment.get("FIREBASE_CLIENT_EMAIL") ?? "",
                   projectId: Environment.get("FIREBASE_PROJECT_ID") ?? "",
-                  key: Environment.get("FIREBASE_PRIVATE_KEY") ?? "")
+                  key: Environment.get("FIREBASE_PRIVATE_KEY")?.replacingOccurrences(of: "\\n", with: "\n") ?? "")
     services.register(fcm, as: FCM.self)
     
     let slackConfig = SlackConfig(basePath: Environment.get("SLACK_WEBHOOK_URL") ?? "")
