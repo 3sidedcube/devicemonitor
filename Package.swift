bff6956b8d505ba755cd435d57650259f16bcc2f
Simon Mitchell <simon@3sidedcube.com>
2019-02-21 09:58:46 +0000

    Revert "Changes ferno to use submodule to build!"
    c2947b40bcb754743762058426492daf641d2ada

--- Package.swift
+++ Package.swift
@@ -10,6 +10,8 @@ let package = Package(
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         
+        .package(url: "https://github.com/3sidedcube/ferno.git", .branch("jwt3")),
+        
         .package(url: "https://github.com/BrettRToomey/Jobs.git", from: "1.1.1"),
         
         .package(url: "https://github.com/MihaelIsaev/FCM.git", from: "0.6.3")
